6e5ac11271f183885071ced01f852a5315507d97
shlies <shlies@github.com>
2024-07-06 20:28:54 +0800

    视觉yyds

--- main.py
+++ main.py
@@ -31,6 +31,15 @@ def pixel_to_camera_coordinates(u, v, d, fx, fy, cx, cy):
     Y = (v - cy) * d / fy
     Z = d
     return X, Y, Z
+def calculate_3d_coordinates(u, v, W_image, W_real, D_known, fx, fy, cx, cy):
+    # 计算深度 Z
+    Z = (D_known * W_real) / W_image
+    
+    # 计算相机坐标系下的三维坐标
+    X = (u - cx) * Z / fx
+    Y = (v - cy) * Z / fy
+    
+    return X, Y, Z
 
 def main():
     pipeline = Pipeline()
@@ -101,21 +110,38 @@ def main():
         depth_image = cv2.applyColorMap(depth_image, cv2.COLORMAP_JET)
         # overlay color image on depth image
         depth_image = cv2.addWeighted(color_image, 0.5, depth_image, 0.5, 0)
-        cv2.imshow("SyncAlignViewer ", depth_image)
-        key = cv2.waitKey(1)
 
         for index, row in detections.iterrows():
             print(f"index: {index} Class: {row['name']}, Confidence: {row['confidence']}, Coordinates: ({row['xmin']}, {row['ymin']}, {row['xmax']}, {row['ymax']})")
+            if(row['confidence']<0.7):
+                continue
             center_x=int((row['xmin']+row['xmax'])/2)
             center_y=int((row['ymin']+row['ymax'])/2)
-            center=[center_y,center_y]
+            center=[center_x,center_y]
             fx = 545.103
             fy = 545.103
             cx = 321.608
             cy = 243.754
             X, Y, Z = pixel_to_camera_coordinates(center_x, center_y, depth_data[center_y][center_x], fx, fy, cx, cy)
-            # translate.trans(center,depth_data)
-            print(f"position: {center_x},{center_y},depth={depth_data[center_y][center_x]} location: X={X}, Y={Y}, Z={Z}")
             
+            fx = 545.103
+            fy = 545.103
+            cx = 321.608
+            cy = 243.754
+
+            # 目标在已知距离下的实际大小
+            W_real = 60  # 目标的宽度
+            D_known = 553  # 已知距离
+
+            # 计算三维坐标
+            X1, Y1, Z1= calculate_3d_coordinates(center_x, center_y, (row['xmax']-row['xmin']), W_real, D_known, fx, fy, cx, cy)
+
+
+            # translate.trans(center,depth_data)
+            cv2.putText(depth_image,f"{int(X)},{int(Y)},{int(Z)}\n{int(X1)},{int(Y1)},{int(Z1)}", center,5,1,(255, 255, 255))
+            print(f"position: {center_x},{center_y},depth={depth_data[center_y][center_x]} location: X={X}, Y={Y}, Z={Z} location_p: X={X1}, Y={Y1}, Z={Z1}")
+        cv2.imshow("SyncAlignViewer ", depth_image)
+        key = cv2.waitKey(1)
+
 if __name__ == "__main__":
     main()
